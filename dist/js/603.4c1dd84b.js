724185fef4923645086a855f7a2421f6c0ab1113
kerwin977 <kerwincode@gmail.com>
2024-08-21 22:59:44 +0800

    Version1.0

---
+++ dist/js/603.4c1dd84b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdemo1"]=self["webpackChunkdemo1"]||[]).push([[603],{603:function(n,u,e){e.r(u),e.d(u,{default:function(){return f}});var t=e(6768);const a={class:"about"},r=(0,t.Lk)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,t.uX)(),(0,t.CE)("div",a,s)}var o=e(1241);const i={},d=(0,o.A)(i,[["render",c]]);var f=d}}]);
+//# sourceMappingURL=603.4c1dd84b.js.map
